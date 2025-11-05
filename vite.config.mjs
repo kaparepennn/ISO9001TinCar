ba3ea6d2e6a8e40e52cb0949a7fe7c580f19a35a
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-11-05 00:54:10 +0000

    cambios

---
+++ vite.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// ⚙️ Configuración para GitHub Pages
+export default defineConfig({
+  plugins: [react()],
+  base: '/iso9001tincar/',
+  build: {
+    outDir: 'dist',
+  },
+})
