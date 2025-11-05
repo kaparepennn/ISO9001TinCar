e6c77b31e3bdd8904ecdba3b69cdfdb6c4fc2b78
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2025-11-05 01:02:40 +0000

    desplkiegue

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// ⚙️ Configuración para GitHub Pages
+// ⚠️ Cambia SOLO si tu repositorio tiene otro nombre exacto
+const repoName = "ISO9001TinCar";
+
 export default defineConfig({
   plugins: [react()],
-  base: '/iso9001tincar/',
+  base: `/${repoName}/`,
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    emptyOutDir: true,
   },
-})
+});
